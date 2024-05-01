f6be0976e1bc9f1d24dcffd1adb4549b8117cebf
catten224 <taegyu224@gmail.com>
2024-05-02 00:47:22 +0900

    [update] [add] Reverse string output using ptr1

--- string_test/string_test/prointer.c
+++ string_test/string_test/prointer.c
@@ -7,8 +7,18 @@ int main()
 {
 	char* ptr1 = "Tech University of Korea";
 
-	// output string using ptr1
+	// string output using ptr1
+	printf("String output using ptr1 : ");
 	for (int i = 0; *(ptr1 + i); i++)
+	{
+		printf("%c", *(ptr1 + i));
+	}
+	printf("\n");
+
+	// Reverse string output using ptr1
+	printf("Reverse string output using ptr1 : ");
+	int len = strlen(ptr1);
+	for (int i = len - 1; *(ptr1 + i); i--)
 	{
 		printf("%c", *(ptr1 + i));
 	}
